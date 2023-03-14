f12b93bda68fa15ab395c202793bf1fe698262dd
Zhixin Li, PhD <zxlee@connect.hku.hk>
2023-03-14 02:08:37 -0400

    Update generateSampleCSV.R

--- generateSampleCSV.R
+++ generateSampleCSV.R
@@ -4,7 +4,7 @@ args = commandArgs(trailingOnly=TRUE)
 # test if there is at least one argument: if not, return an error
 if (length(args)!=1) {
   stop("Usage: Rscript generateSampleCSV.R <fastq_dir>", call.=FALSE)
-} 
+}
 
 fastq_dir <- args[1]
 
@@ -15,9 +15,9 @@ tmp.all <- list.files(fastq_dir)
 tmp.fqs <- tmp.all[grep("fq|fastq", tmp.all)]
 
 tmp.format <- ""
-if (sum(!grepl("fastq", tmp.all)) == 0) {
+if (sum(!grepl("fastq", tmp.fqs)) == 0) {
     tmp.format <- "fastq"
-} else if (sum(!grepl("fq", tmp.all)) == 0) {
+} else if (sum(!grepl("fq", tmp.fqs)) == 0) {
     tmp.format <- "fq"
 } else {
     stop("Unknown format!!! file name should be fastq or fq!!!")
@@ -33,10 +33,11 @@ if (sum(!c(fq1, fq2) %in% tmp.fqs) != 0) {
     stop("fq1 and fq2 generation error!!!")
 }
 
-sampleName <- unlist(lapply(tmp.fqs, function(x) {
+sampleName <- unlist(lapply(fq1, function(x) {
     strsplit(x, split = "_")[[1]][1]
 }))
 
-SampleCSV <- data.frame(sampleName=sampleName, fq1=paste(getwd(),fq1,sep = "/"), fq2=paste(getwd(),fq2,sep = "/"))
+SampleCSV <- data.frame(sampleName=sampleName, fq1=paste(getwd(),fastq_dir,fq1,sep = "/"),
+                        fq2=paste(getwd(),fastq_dir,fq2,sep = "/"))
 
-write.table(SampleCSV, file = "all.sample.csv", row.names = F, col.names = F, quote = F, sep = ",")
+write.table(SampleCSV, file = paste0(fastq_dir,".sample.csv"), row.names = F, col.names = F, quote = F, sep = ",")
